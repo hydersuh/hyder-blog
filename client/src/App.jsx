59b573e6df4e9ae29a43488e80b22f7ccdc30cf6
hydersuh <hydersoft@yahoo.com>
2026-03-30 16:04:31 +0200

    Add routing and new pages to the application
    - Implemented React Router for navigation
    - Created pages: Home, About, Dashboard, SignIn, SignUp, and Projects
    - Updated package.json and package-lock.json to include react-router-domed Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
 
 export default function App() {
-  return(
-    <div className="text-3xl text-red-950">app</div>
-  ) 
-  
-  
-  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
