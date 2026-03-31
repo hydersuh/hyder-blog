0c1be9cfcf9592c4f7f56943b9eacf782618ad9e
hydersuh <hydersoft@yahoo.com>
2026-03-31 21:57:41 +0200

    Create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/routUser.js";
+import authRoutes from "./routes/authRoute.js";
 
 dotenv.config();
 
@@ -17,8 +18,11 @@ mongoose
 const app = express();
 const PORT = 4000;
 
+app.use(express.json());
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
